d8615b879c9029c79f2c37fc474cf6fa7dc9a89e
GiovanniSilva7 <81999742+GiovanniSilva7@users.noreply.github.com>
2024-07-23 22:10:33 -0300

    Delete WeatherTrackingAPI directory

--- WeatherTrackingAPI/models/RegisterModel.cs
+++
@@ -1,7 +0,0 @@
-public class RegisterModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
-    public string PhotoUrl { get; set; }
-}
--- WeatherTrackingAPI/models/WeatherCondition.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class WeatherCondition 
-{
-    public string Location {  get; set; }
-    public string Condition { get; set; }
-    public DateTime? Timestamp { get; set; }
-    public bool IsAlert { get; set; }
-}
